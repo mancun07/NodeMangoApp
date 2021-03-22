bb38ddcfda5217bd1de33fe9cc7054f6806a769f
Dima <address>
2021-03-22 13:44:32 +0300

    XXX

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.DB_URI , {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        })
-        console.log('Mongo DB is connected')
-    }
-    catch (err) {
-        console.log(`error: ${err.message}`)
-    }
-}
+// const connectDB = async () => {
+//     try {
+//         const conn = await mongoose.connect(process.env.DB_URI , {
+//             useNewUrlParser: true,
+//             useCreateIndex: true,
+//             useUnifiedTopology: true
+//         })
+//         console.log('Mongo DB is connected')
+//     }
+//     catch (err) {
+//         console.log(`error: ${err.message}`)
+//     }
+// }
 
-module.exports = connectDB;
+// module.exports = connectDB;
 
 
